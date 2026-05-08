9dc936f6bf85923a5ccc2b4357c6bf8ac467c605
nai-777 <isahbas4@gmail.com>
2026-05-08 14:15:01 -0400

    Estructura inicial spring boot

---
+++ src/main/java/com/evaluacion_nai/hito3/Hito3Application.java
@@ -0,0 +1,13 @@
+package com.evaluacion_nai.hito3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hito3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hito3Application.class, args);
+	}
+
+}
